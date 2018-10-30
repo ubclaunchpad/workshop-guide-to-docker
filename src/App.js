470847e16f7e3eeb185994d00cb9fc7e422c91d6
Robert Lin <robertlin1@gmail.com>
2018-10-30 12:26:42 -0700

    Add Launch Pad flavouring

--- src/App.js
+++ src/App.js
@@ -7,12 +7,28 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <h2 className="App-launchpad">
+            Workshop: A Developer's Guide to Docker
+          </h2>
+          <br />
+          <br />
           <img src={logo} className="App-logo" alt="logo" />
+          <br />
+          <h1 className="App-launchpad">UBC LAUNCH PAD</h1>
+          <h3 className="App-launchpad">SOFTWARE ENGINEERING TEAM</h3>
+          <br />
+          <p>
+            Repository:&nbsp;
+            <code>
+              <a href="https://github.com/ubclaunchpad/workshop-guide-to-docker" target="_blank" rel="noopener noreferrer">
+              ubclaunchpad/workshop-guide-to-docker
+              </a>
+            </code>
+          </p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
